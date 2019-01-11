76e9be14b94f7b767a394565cfc4c4ec00546178
Matt Wenger <matthewwenger@live.com>
2019-01-11 15:29:31 -0800

    Create setup-guacamole.sh

---
+++ setup-guacamole.sh
@@ -0,0 +1,4 @@
+sudo yum update -y
+sudo yum install cairo-devel libjpeg-devel libpng-devel uuid-devel freerdp-devel pango-devel libssh2-devel libssh-dev tomcat tomcat-admin-webapps tomcat-webapps
+wget -O "guacamole-server-1.0.0.tar.gz" 'http://apache.org/dyn/closer.cgi?action=download&filename=guacamole/1.0.0/source/guacamole-server-1.0.0.tar.gz'
+tar zxf guacamole-server-1.0.0..tar.gz
